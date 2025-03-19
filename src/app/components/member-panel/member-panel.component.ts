b3ffa6c2412068b4fde6e0d085219af1fa5249b3
Franklin <franklinpereira98@hotmail.com>
2025-03-19 19:42:52 -0300

    Add Member panel component

---
+++ src/app/components/member-panel/member-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-panel',
+  imports: [],
+  templateUrl: './member-panel.component.html',
+  styleUrl: './member-panel.component.scss'
+})
+export class MemberPanelComponent {
+
+}
