59b1ae53404928219625ec060e22e9423bfe8ea9
Franklin <franklinpereira98@hotmail.com>
2025-03-30 19:21:27 -0300

    Change entry point do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import {ClanHeaderComponent} from './components/clan-header/clan-header.component';
+import {MemberClanListComponent} from './components/member-clan-list/member-clan-list.component';
+import {MemberPanelComponent} from './components/member-panel/member-panel.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {TankListService} from './service/tank-list/tank-list.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ClanHeaderComponent, MemberClanListComponent, MemberPanelComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'qualy';
+
+  constructor(public tankListService: TankListService) {}
+
+  ngOnInit() {
+    this.tankListService.initializeTier10Tanks()
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {AppComponent} from './app.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: 'app', pathMatch: 'full' },
+  { path: 'app', component: AppComponent }
 ];
