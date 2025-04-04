646c74dc30fbd73952470052f47f5888ba809cc0
Franklin <franklinpereira98@hotmail.com>
2025-04-04 19:45:18 -0300

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
 import {AppComponent} from './app.component';
 
 export const routes: Routes = [
