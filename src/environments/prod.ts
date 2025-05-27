ef40e947caf197d103cadb4a39d8452fe1de4c63
Franklin <franklinpereira98@hotmail.com>
2025-05-27 12:12:19 -0300

    Updating secrets

--- src/environments/prod.ts
+++ src/environments/prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   apiKey: process.env['API_KEY'],
   apiBaseUrl: process.env['API_BASE_URL'],
   tanksUrl: process.env['TANK_URL'],
-  playersUrl: process.env['PLAYER_URL'],
+  clanInfoUrl: process.env['CLAN_URL'],
+  playersUrl: process.env['PLAYER_URL']
 };
